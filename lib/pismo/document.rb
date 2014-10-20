4668b00685ef3547c3650cd68c39f77f244aaed7
Alvise Susmel <alvise@alvise.local>
2014-10-20 14:52:17 +0100

    from open() to net/http

--- lib/pismo/document.rb
+++ lib/pismo/document.rb
@@ -2,6 +2,8 @@
 require 'pismo/internal_attributes'
 require 'pismo/external_attributes'
 
+require 'net/http'
+
 module Pismo
 
   # Pismo::Document represents a single HTML document within Pismo
@@ -35,7 +37,7 @@ module Pismo
       @url = handle if handle =~ /\Ahttp/i
 
       @html = if handle =~ /\Ahttp/i
-                open(handle, allow_redirections: :all) { |f| @url = f.base_uri.to_s; f.read }
+                fetch @url
               elsif handle.is_a?(StringIO) || handle.is_a?(IO) || handle.is_a?(Tempfile)
                 handle.read
               else
@@ -49,5 +51,22 @@ module Pismo
 
       @doc.match([*args], all)
     end
+
+    def fetch(uri_str, limit = 20)
+      
+      raise ArgumentError, 'HTTP redirect too deep' if limit == 0
+
+      url = URI.parse(uri_str)
+      @url = uri_str
+      req = Net::HTTP::Get.new(url.path, { 'User-Agent' => ua })
+      response = Net::HTTP.start(url.host, url.port) { |http| http.request(req) }
+      case response
+      when Net::HTTPSuccess     then response
+      when Net::HTTPRedirection then fetch(response['location'], limit - 1)
+      else
+        response.error!
+      end
+    end
+  
   end
 end
